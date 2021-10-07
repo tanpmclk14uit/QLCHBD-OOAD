9ce08bca6f7e60b5df566a1076c788d9f25d4773
Bùi Dương Duy Khang <81902132+khangbdd@users.noreply.github.com>
2021-10-07 23:36:13 +0700

    done

---
+++ QLCHBD-OOAD/Components/BorrowConfirmCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLCHBD_OOAD.Components
+{
+    /// <summary>
+    /// Interaction logic for BorrowConfirmCard.xaml
+    /// </summary>
+    public partial class BorrowConfirmCard : UserControl
+    {
+        public BorrowConfirmCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
