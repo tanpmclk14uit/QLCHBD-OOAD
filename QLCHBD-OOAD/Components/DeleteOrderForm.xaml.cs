f49c4d3c5517636ba38b3e4e054bad19a7779ea2
Bùi Dương Duy Khang <81902132+khangbdd@users.noreply.github.com>
2021-10-07 20:59:30 +0700

    done 1 form

--- QLCHBD-OOAD/Components/DeleteOrderForm.xaml.cs
+++ QLCHBD-OOAD/Components/DeleteOrderForm.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace QLCHBD_OOAD.layout.Order.Forms
+namespace QLCHBD_OOAD.Components
 {
     /// <summary>
     /// Interaction logic for DeleteOrderForm.xaml
