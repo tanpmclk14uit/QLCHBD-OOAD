293ed46e18e68468bb4c159d9f030b601af49dac
eNViDAT-student <nvdat.ngvan@gmail.com>
2021-10-10 13:53:16 +0700

    Add Dashboard

---
+++ QLCHBD-OOAD/diskView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLCHBD_OOAD
+{
+    /// <summary>
+    /// Interaction logic for diskDetail.xaml
+    /// </summary>
+    public partial class diskView : Window
+    {
+        public diskView()
+        {
+            InitializeComponent();
+        }
+
+        private void bttDivorce_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bttPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bttSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bttCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
