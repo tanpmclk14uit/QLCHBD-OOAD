335bdbf85e0e2822d629c27e4c2a5318d80d2c4d
tanpmclk14uit <tan.lk16.cla@gmail.com>
2021-11-07 03:05:03 +0700

    done week task

---
+++ QLCHBD-OOAD/dao/GuestReponsitory.cs
@@ -0,0 +1,43 @@
+﻿using QLCHBD_OOAD.model.Guest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCHBD_OOAD.dao
+{
+    class GuestReponsitory
+    {
+        private Db db;
+        private static GuestReponsitory instance;
+
+        public static GuestReponsitory getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new GuestReponsitory();
+            }           
+            return instance;
+        }
+
+        private GuestReponsitory()
+        {
+            db = Db.getInstace();
+        }
+
+        public Guest findRentalGuestById(string id)
+        {
+            Guest guest = null;
+            string command = $"SELECT `id`, `cmnd_cccd`, `address`, `name` FROM `guest` WHERE id = {id}";
+            var reader = db.executeCommand(command);
+            if (reader != null && reader.Read())
+            {
+                guest = new Guest((long)reader[0], (string)reader[1], (string)reader[2], (string)reader[3]);
+            }
+            db.closeConnection();
+            return guest;
+        }
+
+    }
+}
---
+++ QLCHBD-OOAD/viewmodel/rental/RentalAddMemberViewModel.cs
@@ -0,0 +1,68 @@
+﻿using QLCHBD_OOAD.appUtil;
+using QLCHBD_OOAD.dao;
+using QLCHBD_OOAD.model.Guest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QLCHBD_OOAD.viewmodel.rental
+{
+    class RentalAddMemberViewModel: BaseViewModel
+    {
+        private static RentalAddMemberViewModel instance;
+        public static RentalAddMemberViewModel getIntance()
+        {
+            if(instance == null)
+            {
+                instance = new RentalAddMemberViewModel();
+            }
+            return instance;
+        }
+        public static event GuestTranferInformation guestTranferInformation;
+        private Guest _guest;
+        public Guest guest
+        {
+            get => _guest;
+            set
+            {
+                _guest = value;
+                OnPropertyChanged("guest");
+            }
+        }
+        public ICommand Confirm { get; set; }
+        private string _keyword;
+        public string keyword
+        {
+            get => _keyword;
+            set
+            {
+                _keyword = value;
+                if (value != null)
+                {
+                    string id = Regex.Replace(value, @"[^0-9]", string.Empty);
+                    if (id != "")
+                    {
+                        guest = findGuestById(id);
+                    }
+                }                
+            }
+        }
+        private GuestReponsitory guestReponsitory;
+        private RentalAddMemberViewModel()
+        {
+            guestReponsitory = GuestReponsitory.getInstance();
+            Confirm = new RelayCommand<object>((p) => { return true; }, (p) => { guestTranferInformation(guest); });
+        }
+        public Guest findGuestById(string id)
+        {
+            Guest guest = guestReponsitory.findRentalGuestById(id);
+            return guest;
+        }
+
+
+    }
+}
