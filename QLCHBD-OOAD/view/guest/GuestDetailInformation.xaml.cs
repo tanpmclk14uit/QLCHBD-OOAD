6786d159d6f479257a082c0fbaf259b80a021bf3
tanpmclk14uit <tan.lk16.cla@gmail.com>
2021-11-16 17:50:21 +0700

    Done detail form view

---
+++ QLCHBD-OOAD/view/guest/GuestDetailInformation.xaml.cs
@@ -0,0 +1,40 @@
+﻿using QLCHBD_OOAD.model.Guest;
+using QLCHBD_OOAD.viewmodel.guest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QLCHBD_OOAD.view.guest
+{
+    /// <summary>
+    /// Interaction logic for GuestDetailInformation.xaml
+    /// </summary>
+    public partial class GuestDetailInformation : Window
+    {
+        public GuestDetailInformation(Guest guest)
+        {
+            InitializeComponent();
+            this.DataContext = GuestDetailViewModel.getInstance();
+            if(guest != null)
+            {
+                GuestDetailViewModel.getInstance().setGuest(guest);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ QLCHBD-OOAD/viewmodel/guest/GuestDetailViewModel.cs
@@ -0,0 +1,68 @@
+﻿using QLCHBD_OOAD.model.Guest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QLCHBD_OOAD.viewmodel.guest
+{
+    class GuestDetailViewModel: BaseViewModel
+    {
+        private static GuestDetailViewModel instance;
+
+        public static GuestDetailViewModel getInstance()
+        {
+            if(instance== null)
+            {
+                instance = new GuestDetailViewModel();
+            }
+            return instance;
+        }
+
+        private Guest _guest;
+        public Guest guest
+        {
+            get => _guest;
+            set
+            {
+                _guest = value;                
+                OnPropertyChanged("guest");
+            }
+        }
+        public void setGuest(Guest guest)
+        {
+            this.guest = guest;
+            if (guest != null)
+            {
+                if (guest.isMember)
+                {
+                    isMember = Visibility.Visible;
+                }
+                else
+                {
+                    isMember = Visibility.Hidden;
+                }
+            }
+        }
+
+        private Visibility _isMember;
+        public Visibility isMember
+        {
+            get
+            {
+                return _isMember;
+            }
+            set
+            {
+                _isMember = value;
+                OnPropertyChanged("isMember");
+            }
+        }
+        private GuestDetailViewModel()
+        {
+
+        }
+    }
+}
